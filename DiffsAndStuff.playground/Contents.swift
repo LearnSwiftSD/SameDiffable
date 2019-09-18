3fabb30f122f480621219240000a8d7646316c69
Stephen Martinez <enharmonicnuance@gmail.com>
2019-09-18 14:53:20 -0700

    Added the Playground

---
+++ DiffsAndStuff.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+// Create `originalValues` Array<Int>
+var originalValues = <#Code: Array of Ints#>
+
+// Create `newValues` Array<Int>
+var newValues = <#Code: Array of Ints#>
+
+// Get the `CollectionDifference<Int>` and assign to `diff`
+let diff = <# Take newValues and call differences from originalValues#>
+
+// Loop and display differences
+<#diff.forEach and switch on it#>
+
+// There are two cases .insert and .remove, which each has offset, element and associatedWith
+
+
+// Create a new collection by applying the differences
+var newCollection = <#Take the originalValues and apply the diff to it#>
+
+// Print em out
+<#print it#>
