d5daec046d490d14a990b11c854ef3426c79f936
Stephen Martinez <enharmonicnuance@gmail.com>
2019-09-19 20:01:56 -0700

    Added the diffable DataSource

--- DiffableColors/Controllers/ColorFavsController.swift
+++ DiffableColors/Controllers/ColorFavsController.swift
@@ -12,53 +12,74 @@ import Combine
 class ColorFavsController: UIViewController {
     
     @IBOutlet weak var colorFavsList: UITableView!
-    var selectedPath = IndexPath(row: 0, section: 0)
-    var colorList = [FavColor]()
     var cancellables = Cancellables()
     
     // MARK: Assign/Create The Diffable DataSource `dataSource`
+    lazy var dataSource = {
+        UITableViewDiffableDataSource<DefaultSection, FavColor>(
+            tableView: colorFavsList,
+            cellProvider: colorFavCell
+        )
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // MARK: Apply the initial Snapshot
-        colorList = AppMemory.shared.favColors
         colorFavsList.delegate = self
-        colorFavsList.dataSource = self
         bindPublishers()
-        
+        dataSource.apply(colorFavSnapshot(AppMemory.shared.favColors), animatingDifferences: false)
     }
     
     // MARK: Create a snapshot provider `colorFavSnapshot`
+    private func colorFavSnapshot(_ newColors: [FavColor]) -> NSDiffableDataSourceSnapshot<DefaultSection, FavColor> {
+        var snapshot = NSDiffableDataSourceSnapshot<DefaultSection, FavColor>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(newColors)
+        return snapshot
+    }
     
     // MARK: Create a Cell Provider `colorFavCell`
+    private var colorFavCell: (UITableView, IndexPath, FavColor) -> UITableViewCell? {
+        return { [weak self] tableView, indexPath, favColor in
+            guard
+                let self = self,
+                let cFavCell = Bundle.main.loadNibNamed(ColorFavCell.sbid,
+                                                        owner: self,
+                                                        options: nil
+                    )?.first as? ColorFavCell
+                else { return nil }
+            
+            cFavCell.colorView.shiftTo(favColor.color)
+            cFavCell.nameLabel.text = favColor.name
+            cFavCell.hexLabel.text = favColor.color.hex
+            return cFavCell
+        }
+    }
     
     // MARK: - Persistence Actions / update with Snapshot updates
     private var saveAction: ([FavColor]) -> Void {
         return { [weak self] in
             guard let self = self else { return }
-            self.colorList = $0
-            let path = [IndexPath(row: 0, section: 0)]
-            self.colorFavsList.insertRows(at: path, with: .left)
+            let snapshot = self.colorFavSnapshot($0)
+            self.dataSource.apply(snapshot)
         }
     }
     
     private var editAction: ([FavColor]) -> Void {
         return { [weak self] in
             guard let self = self else { return }
-            self.colorList = $0
-            self.colorFavsList.reloadRows(at: [self.selectedPath], with: .fade)
+            let snapshot = self.colorFavSnapshot($0)
+            self.dataSource.apply(snapshot)
         }
     }
     
     private var deleteAction: ([FavColor]) -> Void {
         return { [weak self] in
             guard let self = self else { return }
-            self.colorList = $0
-            self.colorFavsList.deleteRows(at: [self.selectedPath], with: .right)
+            let snapshot = self.colorFavSnapshot($0)
+            self.dataSource.apply(snapshot)
         }
     }
-    
     
     func bindPublishers() {
         AppMemory.shared.didSave
@@ -88,30 +109,12 @@ extension ColorFavsController: UITableViewDelegate {
 
     // MARK: Update with new DiffableDataSource API
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedPath = indexPath
         tableView.deselectRow(at: indexPath, animated: true)
-        ColorEditController.activate(from: self, setToColor: colorList[indexPath.row])
+        guard let color = dataSource.itemIdentifier(for: indexPath) else { return }
+        ColorEditController.activate(from: self, setToColor: color)
     }
 
-}
-
-extension ColorFavsController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return colorList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let colorFavCell =
-            Bundle.main.loadNibNamed(ColorFavCell.sbid,owner: self, options: nil)?.first as? ColorFavCell else {
-            return UITableViewCell(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
-        }
-        colorFavCell.colorView.shiftTo(colorList[indexPath.row].color)
-        colorFavCell.nameLabel.text = colorList[indexPath.row].name
-        colorFavCell.hexLabel.text = colorList[indexPath.row].color.hex
-        return colorFavCell
-    }
-    
 }
 
 // MARK: - Create DiffableDataSource TableView Sections
+enum DefaultSection { case main }
--- DiffsAndStuff.playground/Contents.swift
+++ DiffsAndStuff.playground/Contents.swift
@@ -1,22 +1,29 @@
 import Foundation
 
 // Create `originalValues` Array<Int>
-var originalValues = <#Code: Array of Ints#>
+var originalValues = [22, 4, 5, 9, 45]
 
 // Create `newValues` Array<Int>
-var newValues = <#Code: Array of Ints#>
+var newValues = originalValues.shuffled()
+
+print("NewValues: \(newValues)")
 
 // Get the `CollectionDifference<Int>` and assign to `diff`
-let diff = <# Take newValues and call differences from originalValues#>
+let diff = newValues.difference(from: originalValues)
 
 // Loop and display differences
-<#diff.forEach and switch on it#>
-
-// There are two cases .insert and .remove, which each has offset, element and associatedWith
-
+// There are two cases `insert` and `remove`, which each has `offset`, `element` and `associatedWith`
+diff.forEach { change in
+    switch change {
+    case .insert(let offset, let element, let associatedWith):
+        print("INSERT -> offset: \(offset), element: \(element), associatedWith: \(String(describing: associatedWith))")
+    case .remove(let offset, let element, let associatedWith):
+        print("REMOVED -> offset: \(offset), element: \(element), associatedWith: \(String(describing: associatedWith))")
+    }
+}
 
 // Create a new collection by applying the differences
-var newCollection = <#Take the originalValues and apply the diff to it#>
+var newCollection = originalValues.applying(diff)
 
 // Print em out
-<#print it#>
+print("New Collection: \(newCollection ?? [])")
